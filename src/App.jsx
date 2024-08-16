a69d626b26c1db1db1d45ace038874f1c735f32a
Menna Ismail <155622935+MennaIsmail30@users.noreply.github.com>
2024-08-16 22:29:07 +0300

    Add fake authentication at context api

--- src/App.jsx
+++ src/App.jsx
@@ -6,32 +6,34 @@ import NotFound from "./pages/PageNotFound";
 import Login from "./pages/Login";
 import AppLayout from "./pages/AppLayout";
 import CityList from "./components/CityList";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
 import { CitiesProvider, useCities } from "./contexts/CitiesContaxt";
-
+import { AuthProvider } from "./contexts/AuthContext";
 function App() {
     return (
-        <CitiesProvider>
-            <BrowserRouter>
-                <Routes>
-                    <Route index element={<HomePage />} />
-                    <Route path="product" element={<Product />} />
-                    <Route path="pricing" element={<Pricing />} />
-                    <Route path="app" element={<AppLayout />}>
-                        <Route index element={<Navigate replace to="cities" />} />
-                        <Route path="cities" element={<CityList />} />
-                        <Route path="cities/:id" element={<City />} />
-                        <Route path="countries" element={<CountryList />} />
-                        <Route path="form" element={<Form />} />
-                    </Route>
-                    <Route path="*" element={<NotFound />} />
-                    <Route path="login" element={<Login />} />
-                </Routes>
-            </BrowserRouter>
-        </CitiesProvider>
+        <AuthProvider>
+            <CitiesProvider>
+                <BrowserRouter>
+                    <Routes>
+                        <Route index element={<HomePage />} />
+                        <Route path="product" element={<Product />} />
+                        <Route path="pricing" element={<Pricing />} />
+                        <Route path="app" element={<AppLayout />}>
+                            <Route index element={<Navigate replace to="cities" />} />
+                            <Route path="cities" element={<CityList />} />
+                            <Route path="cities/:id" element={<City />} />
+                            <Route path="countries" element={<CountryList />} />
+                            <Route path="form" element={<Form />} />
+                        </Route>
+                        <Route path="*" element={<NotFound />} />
+                        <Route path="login" element={<Login />} />
+                    </Routes>
+                </BrowserRouter>
+            </CitiesProvider>
+        </AuthProvider>
     );
 }
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./Login.module.css";
+import { useAuth } from "../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
+import Button from "../components/Button";
+import PageNav from "../components/PageNav";
 
 export default function Login() {
     // PRE-FILL FOR DEV PURPOSES
     const [email, setEmail] = useState("jack@example.com");
     const [password, setPassword] = useState("qwerty");
-
+    const { login, isAuthenticated } = useAuth();
+    const navigate = useNavigate();
+    useEffect(
+        function () {
+            if (isAuthenticated === true) navigate("/app", { replace: true });
+        },
+        [isAuthenticated, navigate]
+    );
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (email && password) {
+            login(email, password);
+        }
+    }
     return (
         <main className={styles.login}>
-            <form className={styles.form}>
+            <PageNav />
+            <form className={styles.form} onSubmit={handleSubmit}>
                 <div className={styles.row}>
                     <label htmlFor="email">Email address</label>
                     <input type="email" id="email" onChange={(e) => setEmail(e.target.value)} value={email} />
@@ -25,7 +43,7 @@ export default function Login() {
                 </div>
 
                 <div>
-                    <button>Login</button>
+                    <Button type="primary">Login</Button>
                 </div>
             </form>
         </main>
